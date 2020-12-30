72ca26937a1167ac60580fe48a95a3cf41d38da0
Kátia Alexsandra Jung <katiajung@outlook.com>
2020-12-30 20:03:27 -0300

    Other entities created

---
+++ EmergencyManagementSystem.Entities/Entities/Emergency.cs
@@ -0,0 +1,13 @@
+﻿using EmergencyManagementSystem.Entities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmergencyManagementSystem.Entities.Entities
+{
+    public class Emergency : Entity<long>
+    {
+        public DateTime Date { get; set; }
+
+    }
+}
