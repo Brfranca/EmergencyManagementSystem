80ebc3f84448e9ca3401ad6fba6810e5c965d51e
Kátia Alexsandra Jung <katiajung@outlook.com>
2021-02-04 09:17:57 -0300

    fix: LoginViewModel

--- EmergencyManagementSystem.Web/Models/LoginViewModel.cs
+++ EmergencyManagementSystem.Web/Models/LoginViewModel.cs
@@ -11,11 +11,11 @@ namespace Microsoft.AspNetCore.Mvc
     {
         [DisplayName("Usuário")]
         [Required(ErrorMessage = "O login do usuário deve ser informado.")]
-        [StringLength(100, MinimumLength = 15, ErrorMessage = "O login do usuário deve ter no mínimo 15 e no máximo 100 caractéres.")]
+        [StringLength(100, MinimumLength = 15, ErrorMessage = "O login do usuário deve ter no mínimo 15 e no máximo 100 caracteres.")]
         public string Login { get; set; }
         [DisplayName("Senha")]
         [Required(ErrorMessage = "A senha do usuário deve ser informada.")]
-        [StringLength(100, MinimumLength = 15, ErrorMessage = "A senha do usuário deve ter no mínimo 8 e no máximo 20 caractéres.")]
+        [StringLength(100, MinimumLength = 15, ErrorMessage = "A senha do usuário deve ter no mínimo 8 e no máximo 20 caracteres.")]
         public string Password { get; set; }
     }
 }
