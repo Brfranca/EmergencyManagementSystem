9943296b3ef42272413404ad47d975001469bbf6
Beatriz França <beatriz.rbfranca@gmail.com>
2021-02-06 20:33:37 -0300

    Feat: Enum - Tela Employee/Register

--- EmergencyManagementSystem.Service/Models/AddressModel.cs
+++ EmergencyManagementSystem.Service/Models/AddressModel.cs
@@ -9,7 +9,6 @@ namespace EmergencyManagementSystem.Service.Models
     public class AddressModel
     {
         public string CEP { get; set; }
-        public string State { get; set; }
         public string City { get; set; }
         public string District { get; set; }
         public string Street { get; set; }
