9bbb39c058bb559bf59d929d7732732b5e396cb8
Beatriz França <beatriz.rbfranca@gmail.com>
2021-02-09 02:17:26 -0300

    --

---
+++ EmergencyManagementSystem.Service/Extension/StringExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmergencyManagementSystem.Service.Extension
+{
+    public static class StringExtension
+    {
+        public static string RemoveMaskCPF(this string cpf)
+        {
+            return cpf.Replace(".", "").Replace("-", "");
+        }
+
+        public static string InsertMaskCPF(this string cpf)
+        {
+            return cpf.Insert(3, ".").Insert(7, ".").Insert(11, "-");
+        }
+
+
+    }
+}
