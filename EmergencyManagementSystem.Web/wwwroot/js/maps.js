bea41102f2ff7f38cbcad8fff2ccca142e388d29
Beatriz França <beatriz.rbfranca@gmail.com>
2021-02-13 03:06:13 -0300

    feat: localization google maps

--- EmergencyManagementSystem.Web/wwwroot/js/maps.js
+++ EmergencyManagementSystem.Web/wwwroot/js/maps.js
@@ -1,5 +1,6 @@
 ﻿
 function initAutocomplete() {
+    debugger;
     const map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: -33.8688, lng: 151.2195 },
         zoom: 13,
