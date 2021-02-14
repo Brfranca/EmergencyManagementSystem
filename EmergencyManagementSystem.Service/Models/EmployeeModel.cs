8789ef5a46bc6f899d6ef89d0a7c400bf8102e7c
Beatriz França <beatriz.rbfranca@gmail.com>
2021-02-14 19:09:39 -0300

    Fix: Attribute AddressModel/ EmployeeModel

--- EmergencyManagementSystem.Service/Models/EmployeeModel.cs
+++ EmergencyManagementSystem.Service/Models/EmployeeModel.cs
@@ -11,18 +11,29 @@ namespace EmergencyManagementSystem.Service.Models
     {
         public long Id { get; set; }
         public Guid Guid { get; set; }
+        [Required(ErrorMessage = "Favor informar o nome completo.")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "O nome deve ter no mínimo 3 e no máximo 100 letras.")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Favor informar o telefone.")]
+        [DataType(DataType.PhoneNumber)]
         public string Telephone { get; set; }
+        [Required(ErrorMessage = "Favor informar o e-mail.")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Favor informar o CPF.")]
+        [StringLength(11, MinimumLength = 11, ErrorMessage = "O CPF deve ter 11 números.")]
         public string CPF { get; set; }
+        [Required(ErrorMessage = "Favor informar o RG.")]
         public string RG { get; set; }
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+        [Range(1, 2, ErrorMessage = "Favor informar a coorporação.")]
         public Company Company { get; set; }
         public string ProfessionalRegistration { get; set; }
         public  AddressModel AddressModel { get; set; }
         public long AddressId { get; set; }
+        [Range(1, 7, ErrorMessage = "Favor informar o cargo ocupacional.")]
         public Occupation Occupation { get; set; }
 
         public EmployeeModel()
