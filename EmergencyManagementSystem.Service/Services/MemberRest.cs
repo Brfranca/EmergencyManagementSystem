22c89a96ee5f82ab18a600d434efe912c3c2ad1e
Kátia Alexsandra Jung <katiajung@outlook.com>
2021-02-14 11:42:05 -0300

    feat: rest, filter, controller e view (member)

---
+++ EmergencyManagementSystem.Service/Services/MemberRest.cs
@@ -0,0 +1,17 @@
+﻿using EmergencyManagementSystem.Service.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmergencyManagementSystem.Service.Services
+{
+    public class MemberRest : RestBase<MemberModel>
+    {
+        public MemberRest(IConfiguration configuration) : base(configuration, "SAMUApi", "Member")
+        {
+        }
+    }
+}
