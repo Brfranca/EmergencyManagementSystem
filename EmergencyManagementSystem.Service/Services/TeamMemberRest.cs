254b52aeb85e56bdfa094cf5971c6c4a9fb0011a
Kátia Alexsandra Jung <katiajung@outlook.com>
2021-02-15 11:16:06 -0300

    feat: view, controller, rest, filter (teamember)

---
+++ EmergencyManagementSystem.Service/Services/TeamMemberRest.cs
@@ -0,0 +1,19 @@
+﻿using EmergencyManagementSystem.Service.Interfaces;
+using EmergencyManagementSystem.Service.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmergencyManagementSystem.Service.Services
+{
+    public class TeamMemberRest : RestBase<TeamMemberModel>, ITeamMemberRest
+    {
+        public TeamMemberRest(IConfiguration configuration)
+            : base(configuration, "SAMUApi", "TeamMember")
+        {
+        }
+    }
+}
