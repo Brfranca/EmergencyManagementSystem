b65b348117b3e10a77afbf0f66ac273d6747f464
Beatriz França <beatriz.rbfranca@gmail.com>
2021-02-19 00:33:13 -0300

    Feat: VehicleManagement View

---
+++ EmergencyManagementSystem.Web/Controllers/VehicleManagementController.cs
@@ -0,0 +1,17 @@
+﻿using EmergencyManagementSystem.Service.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmergencyManagementSystem.Web.Controllers
+{
+    public class VehicleManagementController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new EmergencyModel());
+        }
+    }
+}
