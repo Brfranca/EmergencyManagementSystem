9b88ea609143966628a1adc0999d7a964ac1ddb6
Beatriz França <beatriz.rbfranca@gmail.com>
2021-02-18 22:10:06 -0300

    Feat: EvaluationController e View

---
+++ EmergencyManagementSystem.Web/Controllers/EvaluationController.cs
@@ -0,0 +1,17 @@
+﻿using EmergencyManagementSystem.Service.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmergencyManagementSystem.Web.Controllers
+{
+    public class EvaluationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new EmergencyModel());
+        }
+    }
+}
